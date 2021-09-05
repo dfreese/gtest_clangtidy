ceb5346c32e5fae02250d9166bc8308b4ce84998
David Freese <freese@verily.com>
2021-09-04 18:26:29 -0700

    remove more

--- run_tidy.sh
+++ run_tidy.sh
@@ -1,14 +1,12 @@
 #!/bin/bash -eux
 
-clang-tidy-12 --warnings-as-errors=* --header-filter=\./.* split_writer_test.cc -- \
+clang-tidy-12 --warnings-as-errors=* split_writer_test.cc -- \
   -std=c++17 \
   -isystem googletest/googlemock \
   -isystem googletest/googlemock/include \
   -isystem googletest/googletest \
   -isystem googletest/googletest/include \
-  -iquote . \
   -iquote googletest \
-  -x c++ \
   -I/usr/lib/clang/12/include \
   -I/usr/local/include \
   -I/usr/include/x86_64-linux-gnu \
