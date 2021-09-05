455059199a90ee054e197a85f604425254e871e3
David Freese <freese@verily.com>
2021-09-04 18:25:04 -0700

    slimmer

--- run_tidy.sh
+++ run_tidy.sh
@@ -1,3 +1,18 @@
 #!/bin/bash -eux
 
-clang-tidy-12 --checks=bugprone-*,google-*,-google-runtime-references --warnings-as-errors=* --header-filter=\./.* split_writer_test.cc -- -U_FORTIFY_SOURCE -fstack-protector -Wall -Wunused-parameter  -fno-omit-frame-pointer -std=c++0x -fPIC -std=c++17 -no-canonical-prefixes -Wno-builtin-macro-redefined -D__DATE__="redacted" -D__TIMESTAMP__="redacted" -D__TIME__="redacted" -isystem external/com_google_googletest/googlemock -isystem googletest/googlemock -isystem googletest/googlemock/include -isystem googletest/googlemock/include -isystem googletest/googletest -isystem googletest/googletest -isystem googletest/googletest/include -isystem googletest/googletest/include -iquote . -iquote googletest -iquote googletest -x c++ -I/usr/lib/clang/12/include -I/usr/local/include -I/usr/include/x86_64-linux-gnu -I/usr/include -I/usr/include/c++/9 -I/usr/include/x86_64-linux-gnu/c++/9 -I/usr/include/c++/9/backward
+clang-tidy-12 --warnings-as-errors=* --header-filter=\./.* split_writer_test.cc -- \
+  -std=c++17 \
+  -isystem googletest/googlemock \
+  -isystem googletest/googlemock/include \
+  -isystem googletest/googletest \
+  -isystem googletest/googletest/include \
+  -iquote . \
+  -iquote googletest \
+  -x c++ \
+  -I/usr/lib/clang/12/include \
+  -I/usr/local/include \
+  -I/usr/include/x86_64-linux-gnu \
+  -I/usr/include \
+  -I/usr/include/c++/9 \
+  -I/usr/include/x86_64-linux-gnu/c++/9 \
+  -I/usr/include/c++/9/backward
